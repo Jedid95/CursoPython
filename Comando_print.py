fc04a6b2052b6976caecb654f57eddf10136d724
Jedid95 <jedid.santos@gmail.com>
2022-10-12 22:47:25 +0100

    Aulas do dia 12/10/2022

---
+++ Comando_print.py
@@ -0,0 +1,12 @@
+print(123456)
+print('Luiz', 'Otávio')
+print('Luiz', 'Otávio', sep='-', end='')
+print('João', 'e', 'Manria', sep='-', end='')
+
+
+'''
+824.176.070-18
+'''
+
+print('824','176','070', sep='.', end='-')
+print('18')
