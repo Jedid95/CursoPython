29084cd318106de8a97c0b0c83f2df8198d2d122
Jedid95 <jedid.santos@gmail.com>
2022-10-13 23:31:00 +0100

    Aula sobre strings

---
+++ Strings(texto)_aspas.py
@@ -0,0 +1,11 @@
+"""
+str - string
+"""
+print('alguma coisa')
+print("aspas duplas")
+print(123456)
+
+print('Essa é uma string(str)')
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+
