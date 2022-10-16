dd853783601bae840f07309591b0cd3e99227a91
Jedid95 <jedid.santos@gmail.com>
2022-10-16 23:46:45 +0100

    aula do dia 16-10-2022

---
+++ tipos_dados_primitivos.py
@@ -0,0 +1,21 @@
+"""
+Tipos de Dados
+str - strings -  textos 'Assim' "Assim"
+int - inteiro - 123445 0 -10 -100 -23000
+float - real/ponto flutuante - 10.5 1.5 -90.54 0.0
+bool - booleano/lógico - True/False 10 == 10
+"""
+
+print('Jedid',type('Jedid'))
+print(10,type(10))
+print(0.10,type(0.10))
+print(10 == 10,type(10 == 10))
+
+#Nome: String
+print('Jedid', type('Jedid'))
+#Idade: int
+print(27, type(27))
+#Altura: float
+print(1.74,type(1.74))
+#É maior de idade: bool
+print(27>18, type(27>18))
