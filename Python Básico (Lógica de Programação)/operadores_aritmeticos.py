1833e35bc02f1364c66d752fb2e9ceeea3d3d8b7
Jedid95 <jedid.santos@gmail.com>
2022-10-19 07:16:18 +0100

    python basico - operadores aritmeticos - 19/10/22

---
+++ Python Básico (Lógica de Programação)/operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+"""
++ - Adição
+- - Subtração
+* - Multiplicação
+/ - Divisão
+// - Divisão inteira
+** - Exponenciação
+% - Módulo - Resto da divisão
+() - Alterar as precedencias nas formulas
+"""
+
+print('Multiplicação *', 10 * 10)
+print('Adição +', 10 + 10)
+print('Subtração -', 10 - 5)
+print('Divisão /', 10 / 2)
+print('Divisão nº inteiro //', 10 // 3)
+print('Exponenciação **', 2 ** 10)
+print('Módulo - Resto da divisão', 4 % 5)
